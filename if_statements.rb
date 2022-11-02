1d391badb62a690b66c384d7e3a1b529ef48808d
Huy Phan <huyphan520@gmail.com>
2022-11-02 18:42:56 -0500

    Update finish last question

--- if_statements.rb
+++ if_statements.rb
@@ -84,5 +84,13 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
+cups_of_flower = 2
+sauce = "true"
+if cups_of_flower >= 2 && sauce == "true"
+  puts "I can make pizza"
+else 
+  puts "I cannot make pizza"
+end
+
 cups_of_flour = 1
 has_sauce = true
