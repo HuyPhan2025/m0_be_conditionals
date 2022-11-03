353c24ebd83f8b5bfeafb36141260282554d9dbc
Huy Phan <huyphan520@gmail.com>
2022-11-03 12:39:28 -0500

    Update code challenge file

---
+++ code_challenges/ruby_medium_code.rb
@@ -0,0 +1,7 @@
+good_driving_record = true
+age = 26
+if good_driving_record == true && age > 25
+  puts "They shoud get a discount on the car rental"
+else 
+  puts "Need better records"
+end
---
+++ code_challenges/ruby_mild_code.rb
@@ -0,0 +1,6 @@
+# odd_even = 11
+# if odd_even % 2 == 0
+#   puts "even"
+# else 
+#   puts "odd"
+# end
